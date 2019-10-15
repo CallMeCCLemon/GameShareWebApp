1e12bf40469112a90583066dbfaed9732ff7842b
Thomas Littlejohn <thomas.e.littlejohn@gmail.com>
2019-10-15 21:07:06 +0900

    Fixing build CSS issues

--- src/components/GameDisplay/GameDisplay.js
+++ src/components/GameDisplay/GameDisplay.js
@@ -4,23 +4,24 @@ import PropTypes from 'prop-types';
 import GameTile from "../GameTile/GameTile";
 import './GameDisplay.scss'
 
+
+
 function GameDisplay(props) {
     const getTiles = (games) => {
         return games.map((game, index) => {
             return(
-                <Cell>
+                <Cell key={index}>
                     <GameTile title={game.title}
                               imageURL={game.imageURL}
                               price={game.price}
-                              rating={game.rating}
-                              key={index}/>
+                              rating={game.rating} />
                 </Cell>
             );
         })
     };
 
     return(
-        <Grid className="grid">
+        <Grid columns="repeat(auto-fit, minmax(250px, 3fr))">
             {getTiles(props.games)}
         </Grid>
     )
